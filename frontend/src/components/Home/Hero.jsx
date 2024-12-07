a3be06305d259c850f7f8660f9cc8e801b43ebaa
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2024-12-07 20:12:02 +0500

    changed colors

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -10,8 +10,8 @@ const Hero = () => {
             <div className='basis-[75%] flex flex-col justify-center'>
                 <div className='flex items-center justify-center  basis-[75%] p-4'>
                     <div>
-                        <h1 className='text-4xl font-bold text-left [text-underline-position:from-font] [text-decoration-skip-ink:none] bg-gradient-to-r from-[#23A7F4] to-[#0D32C5] text-transparent bg-clip-text'>FAST-NUCES PRESENTS,</h1>
-                        <h1 className=' text-[9rem] font-bold leading-[120px] text-left [text-underline-position:from-font] [text-decoration-skip-ink:none] bg-gradient-to-r from-[#0D32C5] to-[#23A7F4] text-transparent bg-clip-text'>PROCOM'25</h1>
+                        <h1 className='text-4xl font-extrabold text-left [text-underline-position:from-font] [text-decoration-skip-ink:none] bg-gradient-to-r from-[#0D32C5] to-[#23A7F4]  text-transparent bg-clip-text'>FAST-NUCES PRESENTS,</h1>
+                        <h1 className=' text-[10rem] font-extrabold leading-[140px] text-left [text-underline-position:from-font] [text-decoration-skip-ink:none] bg-gradient-to-r from-[#23A7F4] to-[#0D32C5] text-transparent bg-clip-text'>PROCOM'25</h1>
                         <div className='flex w-full flex-col items-end mt-5'>
                             <p className=' text-[20px] font-extrabold leading-[25.2px] tracking-[0.07em] text-left [text-underline-position:from-font] [text-decoration-skip-ink:none]'>READY TO COMPETE?</p>
                             <Link to="/register">
