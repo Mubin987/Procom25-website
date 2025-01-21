c10df6e491a04f8389e9487fe23d530faf9ed4d9
Mubin <mubinfarid987@gmail.com>
2025-01-21 18:35:13 +0500

    Module sequence & Footer updated

--- frontend/src/pages/ModulePage.jsx
+++ frontend/src/pages/ModulePage.jsx
@@ -11,7 +11,7 @@ const ModulePage = () => {
     const { moduleId } = useParams();
 
     const fetchCompetition = async () => {
-            axios.get(`http://localhost:3000/competitionByName/${moduleId}`)
+            axios.get(`http://procom25-server.vercel.app/competitionByName/${moduleId}`)
             .then((res) => {
                 setCompetitions(res.data)
             })
