5ea102695674cda4a8a65900f43ac1b7ded4e8a1
Mubin <mubinfarid987@gmail.com>
2025-01-23 02:07:07 +0500

    vite.config.js updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     }
   },
-  base: "/", // Set this to "/" or your custom deployment path
+  //base: "/", // Set this to "/" or your custom deployment path
 })
 
