9aee583818438f58318b5bc70126a77c9a3bdc6f
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2025-01-25 23:18:32 +0500

    add sponsors section

--- frontend/src/components/Home/Sponsors.jsx
+++ frontend/src/components/Home/Sponsors.jsx
@@ -4,75 +4,67 @@ import { cn } from "@/lib/utils";
 import Marquee from "@/components/ui/marquee";
 import { Link } from 'react-router-dom';
 
-const reviews = [
+const logos = [
     {
         name: "Jack",
-        username: "@jack",
-        body: "I've never seen anything like this before. It's amazing. I love it.",
-        img: "https://avatar.vercel.sh/jack",
+        img: "https://res.cloudinary.com/drrz1wz3s/image/upload/v1737827015/10_b4mkj4.png",
     },
     {
         name: "Jill",
-        username: "@jill",
-        body: "I don't know what to say. I'm speechless. This is amazing.",
-        img: "https://avatar.vercel.sh/jill",
+        img: "https://res.cloudinary.com/drrz1wz3s/image/upload/v1737827015/9_lvbeer.png",
     },
     {
         name: "John",
-        username: "@john",
-        body: "I'm at a loss for words. This is amazing. I love it.",
-        img: "https://avatar.vercel.sh/john",
+        img: "https://res.cloudinary.com/drrz1wz3s/image/upload/v1737827015/6_rjjnch.png",
     },
     {
         name: "Jane",
-        username: "@jane",
-        body: "I'm at a loss for words. This is amazing. I love it.",
-        img: "https://avatar.vercel.sh/jane",
+        img: "https://res.cloudinary.com/drrz1wz3s/image/upload/v1737827015/4_bzoqdf.png",
     },
     {
         name: "Jenny",
-        username: "@jenny",
-        body: "I'm at a loss for words. This is amazing. I love it.",
-        img: "https://avatar.vercel.sh/jenny",
+        img: "https://res.cloudinary.com/drrz1wz3s/image/upload/v1737827015/3_iq10fv.png",
     },
     {
         name: "James",
-        username: "@james",
-        body: "I'm at a loss for words. This is amazing. I love it.",
-        img: "https://avatar.vercel.sh/james",
+        img: "https://res.cloudinary.com/drrz1wz3s/image/upload/v1737827014/2_b4hfej.png",
+    },
+    {
+        name: "James",
+        img: "https://res.cloudinary.com/drrz1wz3s/image/upload/v1737827014/8_njt5q3.png",
+    },
+    {
+        name: "James",
+        img: "https://res.cloudinary.com/drrz1wz3s/image/upload/v1737827014/7_em3yak.png",
+    },
+    {
+        name: "James",
+        img: "https://res.cloudinary.com/drrz1wz3s/image/upload/v1737827014/1_p8o6tq.png",
+    },
+    {
+        name: "James",
+        img: "https://res.cloudinary.com/drrz1wz3s/image/upload/v1737827014/5_mpcba6.png",
     },
 ];
 
-const firstRow = reviews.slice(0, reviews.length / 2);
-const secondRow = reviews.slice(reviews.length / 2);
+const firstRow = logos.slice(0, logos.length / 2);
+const secondRow = logos.slice(logos.length / 2);
 
 
 const ReviewCard = ({
     img,
-    name,
-    username,
-    body,
 }) => {
     return (
         <figure
             className={cn(
-                "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
-                // light styles
+                "relative w-fit cursor-pointer overflow-hidden rounded-xl border p-2",
                 "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-                // dark styles
                 "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
             )}
         >
-            <div className="flex flex-row items-center gap-2">
-                <img className="rounded-full" width="32" height="32" alt="" src={img} />
-                <div className="flex flex-col">
-                    <figcaption className="text-sm font-medium dark:text-white">
-                        {name}
-                    </figcaption>
-                    <p className="text-xs font-medium dark:text-white/40">{username}</p>
-                </div>
+            <div className="flex items-center w-full h-full justify-center">
+                <img className="w-[100px]" width="1000" height="1000" alt="" src={img} />
             </div>
-            <blockquote className="mt-2 text-sm">{body}</blockquote>
         </figure>
     );
 };
@@ -80,8 +72,7 @@ const ReviewCard = ({
 const Sponsors = () => {
     return (
         <section className='w-full flex flex-col items-center justify-center gap-6 my-5 p-4 overflow-hidden'>
-            <Heading1 text='OUR SPONSORS' />
-            {/* <div className='bg-[linear-gradient(90deg,_#1F95ED_0%,_#2169D4_100%)]'> */}
+            <h1 className='font-extrabold font-lemonmilk w-4/5 bg-clip-text text-transparent bg-[linear-gradient(90deg,_#434242_25.4%,_#2C2C2C_78.4%,_#161616_100%)] text-4xl sm:text-5xl py-2 text-center'>Who attends <span className='font-bold bg-clip-text text-transparent bg-[linear-gradient(180deg,_#199DDF_0%,_#145BD5_100%)] text-4xl sm:text-6xl'>PROCOM</span>?</h1>
             <div className=''>
                 <Marquee pauseOnHover className="[--duration:20s]">
                     {firstRow.map((review) => (
